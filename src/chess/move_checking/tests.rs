3aa4425bfcb3cd44e010fb94fdc0f842b143506e
Matthias Roshardt <marosini@bluewin.ch>
2024-02-20 11:37:34 +0100

    finish tests

--- src/chess/move_checking/tests.rs
+++ src/chess/move_checking/tests.rs
@@ -1,3 +1,4 @@
+use std::result;
 
 use rstest::rstest;
 
@@ -315,40 +316,248 @@ fn test_promotion_capture() {
     );
 }
 
-#[test]
-fn test_sliding_piece_blocked_by_friendly_piece() {}
+#[rstest]
+#[case(Bishop, D, _4, F, _6)]
+#[case(Rook, A, _5, H, _5)]
+#[case(Queen, B, _8, H, _2)]
+#[case(Queen, E, _7, E, _2)]
+fn test_sliding_piece_blocked_by_friendly_piece(
+    #[case] piece: PieceType,
+    #[case] src_file: File,
+    #[case] src_rank: Rank,
+    #[case] dst_file: File,
+    #[case] dst_rank: Rank,
+) {
+    let mut board = Board::empty();
+    board.active_player = White;
+    board.set_piece(src_file, src_rank, Piece(piece, White));
+    board.set_piece(E, _5, Piece(PieceType::Pawn, White));
+    let move_ = Move::Normal {
+        from: Position(src_file, src_rank),
+        to: Position(dst_file, dst_rank),
+    };
 
-fn test_long_slide_blocked_by_opponent_piece() {}
+    assert!(!is_move_legal(&board, &move_));
+}
+
+#[rstest]
+#[case(Bishop, D, _4, F, _6)]
+#[case(Rook, A, _5, H, _5)]
+#[case(Queen, B, _8, H, _2)]
+#[case(Queen, E, _7, E, _2)]
+fn test_long_slide_blocked_by_opponent_piece(
+    #[case] piece: PieceType,
+    #[case] src_file: File,
+    #[case] src_rank: Rank,
+    #[case] dst_file: File,
+    #[case] dst_rank: Rank,
+) {
+    let mut board = Board::empty();
+    board.active_player = Black;
+    board.set_piece(src_file, src_rank, Piece(piece, Black));
+    board.set_piece(E, _5, Piece(PieceType::Pawn, White));
+    let move_ = Move::Normal {
+        from: Position(src_file, src_rank),
+        to: Position(dst_file, dst_rank),
+    };
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_must_not_move_king_into_check() {}
+fn test_must_not_move_king_into_check() {
+    let mut board = Board::empty();
+    board.set_piece(E, _1, Piece(PieceType::King, White));
+    board.set_piece(F, _8, Piece(PieceType::Rook, Black));
+    let move_ = Move::Normal {
+        from: Position(E, _1),
+        to: Position(F, _2),
+    };
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_must_not_leave_king_in_check() {}
+fn test_must_not_leave_king_in_check() {
+    let mut board = Board::empty();
+    board.set_piece(E, _1, Piece(PieceType::King, White));
+    board.set_piece(E, _8, Piece(PieceType::Rook, Black));
+    board.set_piece(G, _2, Piece(PieceType::Bishop, White));
+    let move_ = Move::Normal {
+        from: Position(G, _2),
+        to: Position(F, _3),
+    };
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_must_not_put_king_in_check() {}
+fn test_must_not_put_king_in_check() {
+    let mut board = Board::empty();
+    board.set_piece(E, _1, Piece(PieceType::King, White));
+    board.set_piece(E, _8, Piece(PieceType::Rook, Black));
+    board.set_piece(E, _4, Piece(PieceType::Bishop, White));
+    let move_ = Move::Normal {
+        from: Position(E, _4),
+        to: Position(F, _3),
+    };
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_en_passant_must_not_put_king_in_check() {}
+fn test_en_passant_must_not_put_king_in_check() {
+    let mut board = Board::empty();
+    board.active_player = White;
+    board.set_piece(H, _5, Piece(PieceType::King, White));
+    board.set_piece(D, _5, Piece(PieceType::Pawn, Black));
+    board.set_piece(C, _5, Piece(PieceType::Pawn, White));
+    board.set_piece(B, _5, Piece(PieceType::Rook, Black));
+    board.en_passant_target = Some(Position(D, _6));
+    let move_ = Move::Normal {
+        from: Position(C, _5),
+        to: Position(D, _6),
+    };
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_castling_must_not_put_king_in_check() {}
+fn test_king_side_castling_must_not_put_king_in_check() {
+    let board = Board::from_fen("6r1/8/8/8/8/8/8/4K2R w K - 0 1").unwrap();
+    let move_ = Move::CastleKingside;
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_castling_must_not_move_king_through_check() {}
+fn test_queen_side_castling_must_not_put_king_in_check() {
+    let board = Board::from_fen("r3k3/8/8/8/8/8/8/2Q5 b q - 0 1").unwrap();
+    let move_ = Move::CastleQueenside;
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_valid_castling() {}
+fn test_king_side_castling_must_not_move_king_through_check() {
+    let board = Board::from_fen("4k2r/8/8/8/8/B7/8/8 b K - 0 1").unwrap();
+    let move_ = Move::CastleKingside;
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_missing_castling_rights() {}
+fn test_queen_side_castling_must_not_move_king_through_check() {
+    let board = Board::from_fen("8/8/8/8/8/5b2/8/R3K3 w Q - 0 1").unwrap();
+    let move_ = Move::CastleQueenside;
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_king_move_voids_castling_rights() {}
+fn test_cannot_castle_out_of_check() {
+    let board = Board::from_fen("4r3/8/8/8/8/8/8/4K2R w K - 0 1").unwrap();
+    let move_ = Move::CastleKingside;
+
+    assert!(!is_move_legal(&board, &move_));
+}
+
+#[rstest]
+#[case(B, Move::CastleQueenside)]
+#[case(C, Move::CastleQueenside)]
+#[case(D, Move::CastleQueenside)]
+#[case(F, Move::CastleKingside)]
+#[case(G, Move::CastleKingside)]
+fn test_castle_blocked(#[case] file: File, #[case] move_: Move) {
+    let mut board = Board::empty();
+    board.castling_rights = 0b1111;
+    board.set_piece(file, _1, Piece(PieceType::Bishop, White));
+
+    assert!(!is_move_legal(&board, &move_));
+}
 
 #[test]
-fn test_rook_move_voids_castling_rights() {}
+fn test_valid_king_side_castling() {
+    let board = Board::from_fen("8/8/8/8/8/8/8/R3K2R w KQ - 0 1").unwrap();
+    let move_ = Move::CastleKingside;
+
+    let result = apply_move(&board, &move_).unwrap();
+
+    assert_eq!(result.get_piece(E, _1), None);
+    assert_eq!(result.get_piece(H, _1), None);
+    assert_eq!(result.get_piece(G, _1), Some(Piece(PieceType::King, White)));
+    assert_eq!(result.get_piece(F, _1), Some(Piece(PieceType::Rook, White)));
+}
 
 #[test]
-fn test_rook_capture_voids_castling_rights() {}
+fn test_valid_queen_side_castling() {
+    let board = Board::from_fen("rr6/8/8/8/8/8/8/R3K2R w KQ - 0 1").unwrap();
+    let move_ = Move::CastleQueenside;
+
+    let result = apply_move(&board, &move_).unwrap();
+
+    assert_eq!(result.get_piece(E, _1), None);
+    assert_eq!(result.get_piece(A, _1), None);
+    assert_eq!(result.get_piece(C, _1), Some(Piece(PieceType::King, White)));
+    assert_eq!(result.get_piece(D, _1), Some(Piece(PieceType::Rook, White)));
+}
+
+#[test]
+fn test_missing_castling_rights() {
+    let board = Board::from_fen("8/8/8/8/8/8/8/R3K2R w - - 0 1").unwrap();
+    let move_ = Move::CastleKingside;
+
+    assert!(!is_move_legal(&board, &move_));
+}
+
+#[test]
+fn test_king_move_voids_white_castling_rights() {
+    let board = Board::from_fen("8/8/8/8/8/8/8/R3K2R w KQkq - 0 1").unwrap();
+    let move_ = Move::Normal {
+        from: Position(E, _1),
+        to: Position(E, _2),
+    };
+
+    let result = apply_move(&board, &move_).unwrap();
+
+    assert_eq!(result.castling_rights, 0b0000);
+}
+
+#[test]
+fn test_king_move_voids_black_castling_rights() {
+    let board = Board::from_fen("r3k2r/8/8/8/8/8/8/8 b KQkq - 0 1").unwrap();
+    let move_ = Move::Normal {
+        from: Position(E, _8),
+        to: Position(E, _7),
+    };
+
+    let result = apply_move(&board, &move_).unwrap();
+
+    assert_eq!(result.castling_rights, 0b0000);
+}
+
+#[test]
+fn test_rook_move_voids_castling_rights() {
+    let board = Board::from_fen("8/8/8/8/8/8/8/R3K2R w KQ - 0 1").unwrap();
+    let move_ = Move::Normal {
+        from: Position(H, _1),
+        to: Position(H, _2),
+    };
+
+    let result = apply_move(&board, &move_).unwrap();
+
+    assert_eq!(result.castling_rights, 0b0100);
+}
+
+#[test]
+fn test_rook_capture_voids_castling_rights() {
+    let board = Board::from_fen("7b/8/8/8/8/8/8/R3K2R b KQ - 0 1").unwrap();
+    let move_ = Move::Normal {
+        from: Position(H, _8),
+        to: Position(A, _1),
+    };
+
+    let result = apply_move(&board, &move_).unwrap();
+
+    assert_eq!(result.castling_rights, 0b1000);
+}
